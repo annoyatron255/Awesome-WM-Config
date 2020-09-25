e0c4cf039697d238f06b22f8b9f06d07a759eac5
annoyatron255 <annoyatron255@gmail.com>
2020-09-25 12:00:44 -0500

    Add icons and mouse support to volume widget

--- feign/widget/volume.lua
+++ feign/widget/volume.lua
@@ -1,7 +1,9 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 local wibox = require("wibox")
+local gears = require("gears")
 local naughty = require("naughty")
+local markup = require("feign.markup")
 
 local volume = {}
 
@@ -13,31 +15,61 @@ local volume_bar = wibox.widget {
 	margins          = 1,
 	paddings         = 1,
 	ticks            = false,
-	widget           = wibox.widget.progressbar
+	buttons = gears.table.join(
+		awful.button({ }, 1, function()
+			volume.toggle_mute()
+		end),
+		awful.button({ }, 3, function()
+			volume.toggle_mic_mute()
+		end),
+		awful.button({ }, 4, function()
+			volume.inc(1)
+		end),
+		awful.button({ }, 5, function()
+			volume.inc(-1)
+		end)
+	),
+	widget = wibox.widget.progressbar
+}
+
+local status_textbox = wibox.widget {
+	font = "Material Design Icons 10",
+	widget = wibox.widget.textbox
 }
 
 local prev_vol
 local prev_playback
 volume.notify = function()
-	awful.spawn.easy_async_with_shell("amixer get Master", function(stdout)
-		local vol, playback = string.match(stdout, "([%d]+)%%.*%[([%l]*)")
+	awful.spawn.easy_async_with_shell("{amixer get Master && amixer get Capture ; }", function(stdout)
+		local vol, playback = string.match(stdout, "Playback [%d]+ %[([%d]+)%%%] %[([%l]*)")
+		local mic_vol, mic_playback = string.match(stdout, "Capture [%d]+ %[([%d]+)%%%] %[([%l]*)")
 
-		if not vol or not playback then return end
+		if not vol or not playback or not mic_vol or not mic_playback then return end
 		vol = tonumber(vol)
+		mic_vol = tonumber(mic_vol)
 
-		if vol ~= prev_volume or playback ~= prev_playback then
+		if vol ~= prev_volume or playback ~= prev_playback or mic_vol ~= prev_mic_vol or mic_playback ~= prev_mic_playback then
 			prev_volume = vol
 			prev_playback = playback
+
+			prev_mic_volume = mic_vol
+			prev_mic_playback = mic_playback
 
 			volume_bar:set_value(vol / 100)
 
 			local text = " Volume - " .. vol .. "%"
+			local status_text = ""
+
 			if vol == 0 or playback == "off" then
-				text = text .. " [M]"
+				status_text = status_text .. utf8.char(0xf0581)
 				volume_bar.color = beautiful.muted_color
 			else
 				volume_bar.color = beautiful.normal_color
 			end
+			if mic_playback == "off" then
+				status_text = status_text .. " " .. utf8.char(0xf036d)
+			end
+			status_textbox:set_markup(markup(beautiful.muted_color, status_text))
 
 			if not volume.notification then
 				volume.notification = naughty.notify({
@@ -50,8 +82,10 @@ volume.notify = function()
 				volume.notification.box:setup {
 					layout = wibox.layout.fixed.vertical,
 					{
-						layout = wibox.layout.fixed.horizontal,
 						volume.notification.textbox,
+						nil,
+						status_textbox,
+						layout = wibox.layout.align.horizontal,
 					},
 					{
 						layout = wibox.layout.fixed.horizontal,
@@ -77,7 +111,7 @@ volume.toggle_mute = function()
 end
 
 volume.toggle_mic_mute = function()
-	awful.spawn("amixer set Capture toggle")
+	awful.spawn.easy_async("amixer set Capture toggle", volume.notify)
 end
 
 return volume
